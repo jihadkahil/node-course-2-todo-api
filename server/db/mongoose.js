230a8b2994d4bf46adbded1578b0c18d7d8e79b5
JihadKahil <jihadkahil.93@gmail.com>
2018-09-27 08:29:16 +0300

    update to online

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-//mongodb://heroku_kslhg7zt:fvr75n0kq2pemog59s2qcqbmcu@ds115283.mlab.com:15283/heroku_kslhg7zt
-mongoose.connect(process.env.MONGO_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
